3066e92b687966d379ea11464ae36c5fe02be4c4
Jonny Quarta <quarta@epsitec.ch>
2022-08-19 19:52:51 +0200

    Add test for getting value

--- test/index.js
+++ test/index.js
@@ -5,10 +5,25 @@ const lib = require("../");
 
 describe("OpenKey", function () {
   it("should open a key", function () {
-    const key = lib.openKey("SOFTWARE\\Microsoft", {
-      hive: lib.hives.HKEY_LOCAL_MACHINE,
+    const key = lib.openKey("SOFTWARE", {
+      hive: lib.HKEY_LOCAL_MACHINE,
     });
 
     assert.ok(key);
+    assert.ok(key._registryKey);
+  });
+});
+
+describe("GetValue", function () {
+  it("should open a key and get a value", function () {
+    const key = lib.openKey("SOFTWARE\\Epsitec\\Cresus Monolith\\Setup", {
+      hive: lib.HKEY_LOCAL_MACHINE,
+    });
+
+    assert.ok(key);
+    assert.ok(key._registryKey);
+
+    const installDir = key.getValue("InstallDir");
+    assert.equal(installDir, "C:\\Program Files\\Cresus");
   });
 });
